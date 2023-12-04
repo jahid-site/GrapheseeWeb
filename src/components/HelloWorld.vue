5186358e7e1ea5adef3fb5e8aab9c7f7f1d482aa
arafat168 <arafathossen1680@gmail.com>
2023-12-05 01:29:36 +0600

    This is my frist commit!

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-img style="height: 400px;" src="@/assets/logo.png" />
+      
+      <v-img  height="150" src="@/assets/graphesee_banner.png" />
+
+      
+
+      <div class="py-10" />
+
+      <v-row class="d-flex align-center justify-center">
+        
+
+        <v-col cols="auto">
+          <v-btn
+            color="primary"
+            href=""
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            target="_blank"
+            variant="flat"
+          >
+            Get Started
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
